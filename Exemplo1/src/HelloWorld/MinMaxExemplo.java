cbec9668fd7a479c04b1ef3946c436b237db4af9
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-12 16:40:26 -0300

    First projects commit

---
+++ Exemplo1/src/HelloWorld/MinMaxExemplo.java
@@ -0,0 +1,44 @@
+package HelloWorld;
+import java.util.Scanner;
+
+public class MinMaxExemplo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1,n2,n3;
+		
+		System.out.println("Entre com  primeiro valor: ");
+		n1 = sc.nextInt();
+		
+		System.out.println("Entre com  segundo valor: ");
+		n2 = sc.nextInt();
+		
+		System.out.println("Entre com  terceiro valor: ");
+		n3 = sc.nextInt();
+		
+		
+		int menor, maior;
+		
+		
+		//Encontra o maior
+		if(n1 >= n2 && n1 >= n3) {
+			maior = n1;
+		} else if(n2 >= n1 && n2 > n3) {
+			maior = n2;
+		} else {
+			maior = n3;
+		}
+		
+		//Encontra o menor 
+		if(n1 <= n2 && n1 <=n3) {
+			menor = n1;
+		} else if(n2 <= n1 && n2 <= n3) {
+			menor = n2;
+		} else {
+			menor = n3;
+		}
+		
+		System.out.println("O maior numero eh: " + maior);
+		System.out.println("O menor numero eh: " + menor);
+	}
+}
